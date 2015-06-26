24d10b9df105267afd6e03cf5f21fbf06adeaffe
Raj Singh <raj@foreversingh.com>
2015-06-26 11:44:42 -0500

    Tried to refactor some of the worst code we ever wrote

---
+++ meaningful_names/meaningful_names.rb
@@ -0,0 +1,39 @@
+# Here's a terrible method!
+def find_random_local_songs
+    random_songs = []
+    if current_users_around.empty?
+      result = "Sorry! No users around right now!"
+    else
+      current_users_around.each do |user|
+        unless user.playlists.empty?
+          @playlist = user.playlists.sample
+          unless @playlist.songs.empty?
+            song = @playlist.songs.sample
+            random_songs << song
+          end
+        end
+      end
+      return random_songs
+    end
+
+# Here's a refactor!
+def no_nearby_users?
+  if current_users_around.empty?
+    raise "Sorry! No users around right now!"
+  end
+end
+
+def find_random_local_songs
+  songs = []
+  current_users_around.each do |user|
+    songs << user.playlists.songs.sample
+  end
+  songs
+end
+
+def build_local_playlist(num_of_songs)
+  unless no_nearby_users?
+    @playlist << num_of_songs.times do { find_random_local_songs }
+  end
+  @playlist.uniq!
+end
