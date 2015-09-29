a68316cbd7f5da8ce8b9eead6bb1f3bbb929ea56
Lesley <lkompare87@gmail.com>
2015-09-29 14:47:26 -0500

    Initial Robot Name Refactor

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -1,26 +1,35 @@
 class NameCollisionError < RuntimeError; end
 
 class Robot
+
+
   attr_accessor :name
 
-  @@registry
 
-  def initialize(args = {})
+  def initialize
     @@registry ||= []
-    @name_generator = args[:name_generator]
-
-    if @name_generator
-      @name = @name_generator.call
-    else
-      generate_char = -> { ('A'..'Z').to_a.sample }
-      generate_num = -> { rand(10) }
-
-      @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
-    end
-
-    raise NameCollisionError, 'There was a problem generating the robot name!' if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
-    @@registry << @name
+    name_generator
+    check_registry
   end
+
+  def name_generator
+    char = -> { ('A'..'Z').to_a.sample }
+    num = -> { rand(10) }
+    self.name = "#{char.call}#{char.call}#{num.call}#{num.call}#{num.call}"
+  end
+
+  def check_registry
+    if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
+     raise NameCollisionError, 'There was a problem generating the robot name!'
+    else
+      add_to_registry
+    end
+  end
+
+  def add_to_registry 
+    @@registry << name
+  end
+
 end
 
 robot = Robot.new
