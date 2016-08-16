91e6b467700bf2b2503bdbf2de2bb59793c09d6c
Elizabeth Alexander <eealexander2@gmail.com>
2016-08-16 11:47:51 -0500

    Fix rubocop errors

--- triangle_facts/triangle_facts.rb
+++ triangle_facts/triangle_facts.rb
@@ -5,6 +5,9 @@ class Triangle
     @side1 = side1
     @side2 = side2
     @side3 = side3
+    @angle_list = { angleA: [2**side2, 2**side3, 2**side1, side2, side3],
+                    angleB: [2**side1, 2**side3, 2**side2, side1, side3],
+                    angleC: [2**side1, 2**side2, 2**side3, side1, side2] }
   end
 
   def equilateral?
@@ -19,27 +22,34 @@ class Triangle
     equilateral? || isosceles? ? false : true
   end
 
-  def recipe_facts
-    triangle_facts = { equilateral? => "This triangle is equilateral", isosceles? => "This triangle is isosceles! Also, that word is hard to type.", scalene? => 'This triangle is scalene and mathematically boring.' }
+  def facts
+    triangle_facts = { equilateral? => "This triangle is equilateral",
+                       isosceles? => "This triangle is isosceles!",
+                       scalene? => 'This triangle is scalene and
+                       mathematically boring.' }
     triangle_facts.each { |key, value| puts value if key }
-    puts 'The angles of this triangle are ' + calculate_angles(side1, side2, side3).join(',')
-    p 'This triangle is also a right triangle!' if calculate_angles(side1, side2, side3).include? 90
   end
 
-  def calculate_angles(a, b, c)
-    angle_list = { angleA: [b, c, a, b, c], angleB: [a, c, b, a, c], angleC: [a, b, c, a, b] }
+  def state_facts
+    p 'The angles of this triangle are ' + angles(@angle_list).join(",")
+    if angles(@angle_list).include? 90
+      p 'This triangle is also a right triangle!'
+    end
+  end
+
+  def angles(angle_list)
     angles = []
-    angle_list.each { |_key, value| angles << (value = radians_to_degrees(Math.acos((value[0]**2 + value[1]**2 - value[2]**2) / (2.0 * value[3] * value[4])))) }
+    angle_list.each do |_k, v|
+      angles << degrees(Math.acos((v[0] + v[1] - v[2]) / (2.0 * v[3] * v[4])))
+    end
     [angles[0], angles[1], angles[2]]
   end
 
-  def radians_to_degrees(rads)
+  def degrees(rads)
     (rads * 180 / Math::PI).round
   end
 end
 
-triangles = [[5, 5, 5], [5, 12, 13]]
-triangles.each do |sides|
-  tri = Triangle.new(*sides)
-  tri.recite_facts
-end
+triangle = Triangle.new(5, 5, 5)
+triangle.facts
+triangle.state_facts
