600b4a714b6c5bba0daddc2488640ab83abbb72e
Elizabeth Alexander <eealexander2@gmail.com>
2016-08-23 09:18:59 -0500

    Add third pass at dat-science challenge

--- data_science/spec/spec.rb
+++ data_science/spec/spec.rb
@@ -2,12 +2,11 @@ require 'calculator.rb'
 require 'parser.rb'
 
 describe 'Calculator' do
-  let(:parser) { Parser.new('spec/test_data.json')}
+  let(:parser) { Parser.new('spec/test_data.json') }
   let(:data_export) { parser.json_parse }
-  let(:group_a) { parser.group_a}
+  let(:group_a) { parser.group_a }
   let(:group_b) { parser.group_b }
-  let(:calculator) { Calculator.new(data_export, group_a, group_b)}
-
+  let(:calculator) { Calculator.new(data_export, group_a, group_b) }
 
   it "calculates sample size of each cohort" do
     x = calculator.cohort_size(group_a)
@@ -21,5 +20,4 @@ describe 'Calculator' do
   it "calculates standard error of each cohort" do
     expect(calculator.standard_error(group_a)).to eq(0.67)
   end
-
 end
